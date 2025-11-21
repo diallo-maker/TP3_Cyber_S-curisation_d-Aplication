6e9e0bef6e432bb48c6554ffd05208548a01fa1e
diallo-maker <1856207@cegepmontpetit.ca>
2025-11-21 18:07:25 -0500

    update
    Correctif #1

---
+++ 3U4-TP3-1.0.0.0/DonneesSecurite.cs
@@ -0,0 +1,47 @@
+﻿// https://crackstation.net/
+// https://www.mscs.dal.ca/~selinger/md5collision/
+
+using System;
+// ajout de la librairie Bcrypt
+using BCrypt.Net;
+
+namespace consoleApp
+{
+    class DonneesSecurite
+    {
+        public static string Encrypter(string input)
+        {
+            string result = "";
+            foreach (char c in input)
+            {
+                char t = (char)(c * 2);
+                result += t;
+            }
+            return result;
+        }
+
+        public static string Decrypter(string input)
+        {
+            string result = "";
+            foreach (char c in input)
+            {
+                char t = (char)(c / 2);
+                result += t;
+            }
+            return result;
+        }
+
+        // fonctions permettant le hachage des mots de passe
+        public static string HacherLeMotDePasse(string input)
+        {
+                return BCrypt.Net.BCrypt.HashPassword(input, workFactor: 12); 
+        }
+        
+        public static bool VerifierLeMotDePasse(string motDePasse, string hache)
+        {
+            // Calcule le hachage MD5 du mot de passe fourni et le compare avec le haché stocké
+            return BCrypt.Net.BCrypt.Verify(motDePasse,hache);
+        }
+        
+    }
+}
