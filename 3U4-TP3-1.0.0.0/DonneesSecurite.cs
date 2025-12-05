1f5568aef972c9270591e0921918d9c2179f29a7
diallo-maker <1856207@cegepmontpetit.ca>
2025-12-05 17:50:26 -0500

    Update

--- 3U4-TP3-1.0.0.0/DonneesSecurite.cs
+++ 3U4-TP3-1.0.0.0/DonneesSecurite.cs
@@ -2,34 +2,39 @@
 // https://www.mscs.dal.ca/~selinger/md5collision/
 
 using System;
+using System.Text;
+using System.Security.Cryptography; 
+using Encryption.Blowfish; // blowfish
 // ajout de la librairie Bcrypt
 using BCrypt.Net;
-
 namespace consoleApp
 {
     class DonneesSecurite
     {
-        public static string Encrypter(string input)
+
+        // ========== BLOWFISH ENCRYPTION ==========
+
+        private static string key = "a3bd614b27864e3f854b971f9df1a802";
+        private static byte[] iv = new byte[] { 23, 56, 45, 67, 78, 89, 90, 12 };
+
+        public static string Encrypter(string source)
         {
-            string result = "";
-            foreach (char c in input)
-            {
-                char t = (char)(c * 2);
-                result += t;
-            }
-            return result;
+            byte[] buffer = Encoding.UTF8.GetBytes(source);
+            buffer = buffer.CopyAndPadIfNotAlreadyPadded();
+            var ctr = new BlowfishCtr(key);
+            ctr.CryptOrDecrypt(buffer, iv);
+            return Convert.ToBase64String(buffer);
         }
 
-        public static string Decrypter(string input)
+        public static string Decrypter(string source)
         {
-            string result = "";
-            foreach (char c in input)
-            {
-                char t = (char)(c / 2);
-                result += t;
-            }
-            return result;
-        }
+            byte[] buffer = Convert.FromBase64String(source);
+            var ctr = new BlowfishCtr(key);
+            ctr.CryptOrDecrypt(buffer, iv);
+            return Encoding.UTF8.GetString(buffer).TrimEnd('\0');
+        }   
+        // ========== BLOWFISH ENCRYPTION FIN ==========
+
 
         // fonctions permettant le hachage des mots de passe
         public static string HacherLeMotDePasse(string input)
